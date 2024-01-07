5ad2a3e7b794841a3eee573344236c5cdb07f50c
SWARAJ PRADHAN <pswaraj96@gmail.com>
2024-01-07 11:01:34 +0530

    first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,79 @@
+/*
+//Assignment-1 (Values and Variables)
+
+let country = "India";
+let continent = "Asia";
+let population = 150;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+*/
+
+/*
+//Assignment-2 (Data Types)
+
+let isIsland = true;
+let language;
+let population = 150;
+let country = "India";
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof population);
+console.log(typeof country);
+
+*/
+
+/*
+//Assignment-3 (let, const and var)
+
+let language = "English";
+const country = "India";
+const continent = "Asia";
+const isIsland = false;
+isIsland = true;  // it will throw an error
+
+*/
+
+//Assignment-4 (Basic Operators)
+let population = 150;
+const country = "India";
+const continent = "Asia";
+let language = "Hindi";
+console.log(population / 2);
+population += 1;
+console.log(population);
+console.log(population > 600);
+console.log(population < 33);
+let description =
+  country +
+  " is in " +
+  continent +
+  ", and it's " +
+  population +
+  " million people speak " +
+  language;
+console.log(description);
+
+/*
+//Challenge 1
+
+const heightOfMark = 1.69;
+const massOfMark = 78;
+const heightOfJohn = 1.95;
+const massOfJohn = 92;
+
+
+const heightOfMark = 1.88;
+const massOfMark = 95;
+const heightOfJohn = 1.76;
+const massOfJohn = 85;
+
+const BMIMark = massOfMark / (heightOfMark * heightOfMark);
+const BMIJohn = massOfJohn / (heightOfJohn * heightOfJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
+
+*/
