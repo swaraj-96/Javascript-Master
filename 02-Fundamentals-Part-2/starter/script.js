57129bb10f08cccbccdc6f0a8da73d13c642acfe
SWARAJ PRADHAN <pswaraj96@gmail.com>
2024-01-10 23:00:07 +0530

    fundamental-2 started

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,14 @@
+// Assignment-1 (Functions)
+
+function describeCountry(country, population, capitalCity) {
+  console.log(
+    `${country} has ${population} million people and its capital city is ${capitalCity}`
+  );
+}
+
+country1 = describeCountry("India", 150, "Delhi");
+console.log(country1);
+country2 = describeCountry("Russia", 50, "Moscow");
+console.log(country1);
+country3 = describeCountry("USA", 80, "Washington");
+console.log(country1);
